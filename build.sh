2ea350ddfbc28f46d315df5937441f5dc71b999c
Bruno Bizeray <brunodsbg@gmail.com>
2025-09-10 20:13:29 -0300

    ajust build sh

--- build.sh
+++ build.sh
@@ -6,12 +6,7 @@ cd iara-server
 
 echo "Building UI"
 cd ../iara-ui
-nvm use
+npm install
 npm run build
 cd ..
-
 echo "Build finished".
-
-docker rm -f iara
-docker build -t iara-app . --no-cache
-docker run -p 9090:80 --name iara iara-app
